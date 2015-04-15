a5b910aedf2654d6617a88ecd6048d1e867a7210
Ilnar Garifullin <garifullin-ilnar2013@yandex.ru>
2015-04-15 21:05:26 +0300

    second commit

--- index.php
+++ index.php
@@ -5,14 +5,17 @@ $name = "Ильнар";
 $age = 28;
 echo 'Меня зовут '.$name.'. Мне '.$age.' лет.';
 
-define("city","Набережные Челны");?>
+unset($name,$age);
+
+
+define("CITY","Набережные Челны");?>
 <br>
 <?php
-if (defined("city")==true) {
-    echo 'Значение константы: '.city.'.';
+if (defined("CITY")) {
+    echo 'Значение константы: '.CITY.'.';
 };
-//city="San Francisco";
 ?>
+
 <br>
 <?php
 $book = array(
@@ -23,21 +26,21 @@ $book["pages"]=188;
 echo 'Недавно я прочитал книгу '.$book["title"]. ' написанную автором '.$book["author"].', я осилил все '.$book["pages"].' страниц, мне она очень понравилась.'
 ?>
 <br>
+
 <?php
-$book = array (
-    "book1" => array (
+$books = array (
+    0=> $book1 =  array(
         "title1" => "\"Путь Кайзен\"",
         "author1" => "Робертом Маурером",
         "pages1" => 188
     ),
-    "book2" => array(
+    1=> $book2 = array(
         "title2" => "\"Поток\"",
         "author2" => "Михай Чиксентмихайи",
         "pages2" => 357
     )
 );
-echo 'Недавно я прочитал книги '.$book["book1"]["title1"].' и '.$book["book2"]["title2"]
-        .', написанная соответственно авторами '.$book["book1"]["author1"].' и '.$book["book2"]["author2"].', я осилил в сумме '
-        .$sum = $book["book1"]["pages1"] + $book["book2"]["pages2"].' страниц.'
+echo 'Недавно я прочитал книги '.$book1["title1"].' и '.$book2["title2"]
+        .', написанная соответственно авторами '.$book1["author1"].' и '.$book2["author2"].', я осилил в сумме '
+        .$sum = $book1["pages1"] + $book2["pages2"].' страниц.'
 ?>
-
